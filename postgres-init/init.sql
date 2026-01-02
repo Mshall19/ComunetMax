8fa985dcb9e7f87a1d8f3fab7040867361d9de55
Mshall19 <juandapeva282@gmail.com>
2026-01-01 22:31:41 -0500

    arregle la conxion con el api gateway y los microservicios

--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -1,4 +1,4 @@
 -- init.sql
-CREATE DATABASE planes_db;
-CREATE DATABASE solicitudes_db;
-CREATE DATABASE usuarios_db;
+CREATE DATABASE db_planes;
+CREATE DATABASE db_empresas;
+CREATE DATABASE db_usuarios;
