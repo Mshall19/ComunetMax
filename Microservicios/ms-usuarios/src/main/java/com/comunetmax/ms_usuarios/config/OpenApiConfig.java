dfb9161c51a8526872a412c7b470d0e23f853d64
Mshall19 <juandapeva282@gmail.com>
2026-03-03 07:45:23 -0500

    configuracion del swagger para la documentacion

---
+++ Microservicios/ms-usuarios/src/main/java/com/comunetmax/ms_usuarios/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.comunetmax.ms_usuarios.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "ComunetMax API", version = "1.0"),
+        // ESTA ES LA LÍNEA CLAVE:
+        servers = { @io.swagger.v3.oas.annotations.servers.Server(url = "http://localhost:8080", description = "Gateway Server") },
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
